e627cf6dd2db2df7876e964bf6955caa19f5fdf8
23vamsi <mvamsisai9996@gmail.com>
2020-10-10 14:18:57 +0530

    added

---
+++ list/list1.py
@@ -0,0 +1,14 @@
+pkmovies=["jalsa","panja","gs",123]
+pkmovies.append("thammudu")
+pkmovies.append("thammudu")
+pkmovies.remove("jalsa")
+print(pkmovies)
+
+num=[1,5,7,9,11,13]
+num[0]=(3)
+print(num)
+
+num=[1,2,3,4,5,6,7]
+num1=[8,9,10]
+num.extend(num1)
+print(num)
---
+++ tuple/tuple1.py
@@ -0,0 +1,6 @@
+num=(2,5,7,9,11)
+print(num[2])
+
+num=(3,5,7,8)
+num[0]=(9)
+print(num)
